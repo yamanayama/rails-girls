093b1a392295ddfd395d6de82aeded9235670b96
yamanayama <ymmtnomail@gmail.com>
2020-02-01 16:55:19 +0900

    画像投稿のバリデーション追加した

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,6 @@
 class Idea < ApplicationRecord
   mount_uploader :picture, PictureUploader
+  validates :picture, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
 end
